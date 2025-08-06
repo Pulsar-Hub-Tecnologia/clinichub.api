c873bd06852529236a05a6cea2ef8164bc4fd578
Diego Ciara <suporte.diegociara@gmail.com>
2025-08-06 17:55:28 -0300

    fix: apply authentication and profile checks to account routes

--- src/routes/account.routes.ts
+++ src/routes/account.routes.ts
@@ -1,12 +1,14 @@
 import Router from 'express';
 import AccountController from '@controllers/AccountController';
+import { ensureAuthenticated } from '@middlewares/ensureAuthenticated';
+import { ensureProfile } from '@middlewares/ensureProfile';
 
 const AccountRoutes = Router();
-AccountRoutes.post('/', AccountController.create);
-AccountRoutes.put('/', AccountController.update);
-AccountRoutes.get('/', AccountController.find);
 AccountRoutes.get('/validate/', AccountController.validate);
-AccountRoutes.get('/accesses/', AccountController.accesses);
-AccountRoutes.post('/accesses/sign-workspace', AccountController.signWorkspace);
+AccountRoutes.post('/',  AccountController.create);
+AccountRoutes.put('/',  ensureAuthenticated, ensureProfile, AccountController.update);
+AccountRoutes.get('/', ensureAuthenticated, ensureProfile, AccountController.find);
+AccountRoutes.get('/accesses/', ensureAuthenticated, ensureProfile, AccountController.accesses);
+AccountRoutes.post('/accesses/sign-workspace', ensureAuthenticated, ensureProfile, AccountController.signWorkspace);
 
 export default AccountRoutes;
